e753c97e50c44f5c0d48e7b25a3bedef2d6436ae
wmxpy <wm@mengw.io>
2023-03-20 22:39:49 -0500

    init rectangle containers

---
+++ src/components/loading-container-rectangle/styles/styled-core-container.ts
@@ -0,0 +1,12 @@
+/**
+ * @author WMXPY
+ * @namespace Components_LoadingContainerRectangle_Styles
+ * @description Styled Core Container
+ */
+
+import styled, { StyledComponent } from "styled-components";
+import { BarkThemeProps } from "../../../theme/declare";
+
+export const LoadingContainerRectangleStyledCoreContainer: StyledComponent<"div", BarkThemeProps, any> =
+    styled.div`
+`;
---
+++ src/components/loading-container-rectangle/styles/styled-front-rectangle.ts
@@ -0,0 +1,12 @@
+/**
+ * @author WMXPY
+ * @namespace Components_LoadingContainerRectangle_Styles
+ * @description Styled Front Rectangle
+ */
+
+import styled, { StyledComponent } from "styled-components";
+import { BarkThemeProps } from "../../../theme/declare";
+
+export const LoadingContainerRectangleStyledFrontRectangle: StyledComponent<"div", BarkThemeProps, any> =
+    styled.div`
+`;
